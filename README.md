76f48ab98496ea2176814cebf2d3675854b60dc8
HatsuSixty <hatsunotsus@gmail.com>
2023-11-26 22:47:29 -0300

    Ready. Set. Go!

---
+++ README.md
@@ -0,0 +1,11 @@
+# OhioTest
+
+A patch for the [minetest](https://github.com/minetest/minetest) engine with OhioCraft specific changes.
+
+## Quick Start
+
+To download minetest and apply the patch, run `build.py`:
+```console
+$ ./build.py
+```
+Then, you can compile the source code in the `minetest` folder by following the instructions in [minetest's repo](https://github.com/minetest/minetest).
---
+++ build.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+
+from subprocess import run
+from shutil import rmtree, copytree, copy
+from sys import exit
+from os import unlink
+from os.path import isdir, isfile
+
+CLONE_URL = "https://github.com/minetest/minetest"
+# CLONE_URL = "https://github.com/rollerozxa/rollertest"
+
+if __name__ == "__main__":
+    print("[INFO] Downloading minetest...")
+    if not isdir("minetest"):
+        if isfile("minetest"):
+            print("[ERROR] Path `minetest` exists and is a file")
+            exit(1)
+        run(["git", "clone", "--depth=1",
+             CLONE_URL, "minetest"])
+    else:
+        print("[WARN] `./minetest` already exists, not redownloading...")
+
+    print("[INFO] Patching `minetest/builtin`")
+    rmtree("./minetest/builtin")
+    copytree("./builtin", "./minetest/builtin")
+
+    print("[INFO] Patching `minetest/src/defaultsettings.cpp`")
+    unlink("./minetest/src/defaultsettings.cpp")
+    copy("./src/defaultsettings.cpp", "./minetest/src/defaultsettings.cpp")
+
+    print("[INFO] Downloading OhioCraft...")
+    ohiocraft_path = "./minetest/games/ohiocraft"
+    if not isdir("./ohiocraft"):
+        if isdir(ohiocraft_path):
+            rmtree(ohiocraft_path)
+        run(["git", "clone", "https://github.com/OhioRP/OhioCraft",
+             "./ohiocraft"])
+        run(["bash", "./build.sh"], cwd="./ohiocraft")
+        copytree("./ohiocraft/Build", ohiocraft_path)
+    else:
+        print(f"[WARN] `{ohiocraft_path}` already exists, not redownloading...")
+
+    irrlicht_path = "./minetest/lib/irrlichtmt"
+    print("[INFO] Downloading Irrlicht...")
+    if not isdir(irrlicht_path):
+        run(["git", "clone", "https://github.com/minetest/irrlicht",
+             irrlicht_path])
+    else:
+        print(f"[WARN] `{irrlicht_path}` already exists, not redownloading...")
+
+    if isdir("./minetest/games/devtest"):
+        print("[INFO] Removing `minetest/games/devtest`...")
+        rmtree("./minetest/games/devtest")
+
+    print("\033[1;32mDONE! Now you can compile minetest in the" +
+          " `./minetest` directory by following the instructions in" +
+          f" `{CLONE_URL}`\033[m")
